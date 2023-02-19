aa70e6a8e9093a69aae1cdbd7f77bdc32e1729c4
Nelson Ongany <harold.nh94@gmail.com>
2023-02-19 11:47:02 +0000

    config

--- _config.js
+++ _config.js
@@ -1,7 +1,7 @@
 var config = {}
 
 // Updated to have your correct username and password
-config.mongoURI = {
+config = {
     production: 'mongodb+srv://haroldnh94:ZpbvEpmfxLxnWdbb@cluster1.bzkjuid.mongodb.net/?retryWrites=true&w=majority',
     development: 'mongodb+srv://haroldnh94:ZpbvEpmfxLxnWdbb@cluster1.bzkjuid.mongodb.net/?retryWrites=true&w=majority',
     test: 'mongodb+srv://haroldnh94:ZpbvEpmfxLxnWdbb@cluster1.bzkjuid.mongodb.net/?retryWrites=true&w=majority',
