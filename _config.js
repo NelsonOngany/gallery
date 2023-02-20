1f7ab9bd490b81ae9397c3b4d777f875bd89e077
Nelson Ongany <harold.nh94@gmail.com>
2023-02-20 11:48:37 +0000

    Changes

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Updated to have your correct username and password
 config = {
-    production: 'mongodb+srv://haroldnh94:ZpbvEpmfxLxnWdbb@cluster1.bzkjuid.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://haroldnh94:ZpbvEpmfxLxnWdbb@cluster1.bzkjuid.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://haroldnh94:ZpbvEpmfxLxnWdbb@cluster1.bzkjuid.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://harold:WUkpuIJwteaX5XRc@cluster0.mlullx6.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://harold:WUkpuIJwteaX5XRc@cluster0.mlullx6.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://harold:WUkpuIJwteaX5XRc@cluster0.mlullx6.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
